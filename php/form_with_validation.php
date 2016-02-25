62f41ce385f0c99bbfd147bccb8a733cec10922e
Bikram Pokhrel <pokhrel2039@yahoo.com>
2016-02-25 17:40:54 -0600

    updated login page for error disply,created encrypt_hash fucntion and password_check functions on functions.php

--- php/form_with_validation.php
+++ php/form_with_validation.php
@@ -1,36 +1,40 @@
-<?php
-  include("validation_functions.php");
-  
-  //session_start();
-if(isset($_POST['submit']))
-{
-  $connection= mysqli_connect('localhost','csci3300_poker','8duK8rat2ehuyedR','csci3300_poker') ;
- if(mysqli_connect_errno()) {
-    die("Database connection failed: " . 
-         mysqli_connect_error() . 
-         " (" . mysqli_connect_errno() . ")"
-    );
-  }
- $username=$_POST['username'];
- $password=$_POST['password'];
- if($username!=''&&$password!='')
- {
-   $query="select * from login where username='".$username."' and password='".$password."'";
-  $search=mysqli_query($connection,$query);
-   $res=mysqli_fetch_row($search);
-   if($res)
-   {
-    $_SESSION['username']=$username;
-    redirect_to("lobby.php");
+<?php require_once("session.php"); ?>
+<?php include("db_connection.php"); ?>
+<?php include("functions.php"); ?>
+<?php require_once("validation_functions.php"); ?>
 
-    }else{
-          echo "Username/password not found.";
-    //echo'You entered username or password is incorrect';
-   }
- }
- else
- {
-  echo'Enter both username and password';
- }
-}
+  <?php
+  
+ $username = "";
+  
+  if (isset($_POST['submit'])) {
+    // form was submitted
+
+  // validations
+  $required_fields = array("username", "password");
+  validate_presences($required_fields);
+
+    if (empty($errors)) {
+       // Attempt Login
+
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+  $found_admin = attempt_login($username, $password);
+    if ($found_admin) {
+      // Success
+      // Mark user as logged in
+     // $_SESSION["admin_id"] = $found_admin["id"];
+      $_SESSION["username"] = $username;
+      redirect_to("lobby.php");
+    } else {
+      // Failure
+      $_SESSION["message"] = "Username/password not found.";
+    }
+  }
+} else {
+    
+} // end: if (isset($_POST['submit']))
+
 ?>
+ 
--- php/validation_functions.php
+++ php/validation_functions.php
@@ -1,13 +1,29 @@
-
 <?php
+$errors = array();
+
+function fieldname_as_text($fieldname) {
+  $fieldname = str_replace("_", " ", $fieldname);
+  $fieldname = ucfirst($fieldname);
+  return $fieldname;
+}
+
 
 // * presence
-// use trim() so empty spaces don't count
-// use === to avoid false positives
 // empty() would consider "0" to be empty
 function has_presence($value) {
 	return isset($value) && $value !== "";
 }
+
+function validate_presences($required_fields) {
+  global $errors;
+  foreach($required_fields as $field) {
+    $value = trim($_POST[$field]);
+  	if (!has_presence($value)) {
+  		$errors[$field] = fieldname_as_text($field) . " can't be blank";
+  	}
+  }
+}
+
 
 // * string length
 // max length
@@ -31,27 +47,4 @@ function validate_max_lengths($fields_with_max_lengths) {
 	}
 }
 
-function form_errors($errors=array()) {
-	$output = "";
-	if (!empty($errors)) {
-	  $output .= "<div class=\"error\">";
-	  $output .= "Please fix the following errors:";
-	  $output .= "<ul>";
-	  foreach ($errors as $key => $error) {
-	    $output .= "<li>{$error}</li>";
-	  }
-	  $output .= "</ul>";
-	  $output .= "</div>";
-	}
-	return $output;
-}
-
 ?>
-<?php
-		function redirect_to($new_location) {
-		header("Location: " . $new_location);
-		exit;
-	}
-	
-?>
-
